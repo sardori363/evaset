739c7730773b8ff15836645bbbef945c1fdfb125
Mukhammadqodir <realkings1007.com>
2022-01-12 12:23:11 +0300

    '''j

---
+++ src/main/java/uz/pdp/springsecurity/entity/Customer.java
@@ -0,0 +1,25 @@
+package uz.pdp.springsecurity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.pdp.springsecurity.entity.template.AbsEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Customer extends AbsEntity {
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String phoneNumber;
+    @Column(nullable = false)
+    private String telegram;
+
+
+}
---
+++ src/main/java/uz/pdp/springsecurity/payload/ApiResponse.java
@@ -0,0 +1,22 @@
+package uz.pdp.springsecurity.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ApiResponse {
+
+    private String message;
+    private boolean success;
+
+    private Object object;
+
+    public ApiResponse(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+}
