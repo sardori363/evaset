59e05ae6bf24b04317424dd32e9920904bcb290d
sardor363 <sardorbekodiljonov363@gmail.com>
2022-01-13 16:16:47 +0500

    measurement controller

--- src/main/java/uz/pdp/springsecurity/controller/MeasurementController.java
+++ src/main/java/uz/pdp/springsecurity/controller/MeasurementController.java
@@ -3,10 +3,7 @@ package uz.pdp.springsecurity.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.pdp.springsecurity.payload.ApiResponse;
 import uz.pdp.springsecurity.payload.CategoryDto;
 import uz.pdp.springsecurity.payload.MeasurementDto;
@@ -25,6 +22,36 @@ public class MeasurementController {
     @PostMapping("/add")
     public HttpEntity<?> add(@RequestBody MeasurementDto measurementDto) {
         ApiResponse apiResponse = measurementService.add(measurementDto);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @PutMapping("/edit/{id}")
+    public HttpEntity<?> edit(@PathVariable Integer id, @RequestBody MeasurementDto measurementDto) {
+        ApiResponse apiResponse = measurementService.edit(id, measurementDto);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @GetMapping("/get/{id}")
+    public HttpEntity<?> get(@PathVariable Integer id) {
+        ApiResponse apiResponse = measurementService.get(id);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @GetMapping("/get")
+    public HttpEntity<?> getAll() {
+        ApiResponse apiResponse = measurementService.getAll();
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public HttpEntity<?> delete(@PathVariable Integer id) {
+        ApiResponse apiResponse = measurementService.delete(id);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @DeleteMapping("/delete")
+    public HttpEntity<?> deleteAll() {
+        ApiResponse apiResponse = measurementService.deleteAll();
         return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
     }
 
--- src/main/java/uz/pdp/springsecurity/service/MeasurementService.java
+++ src/main/java/uz/pdp/springsecurity/service/MeasurementService.java
@@ -17,6 +17,38 @@ public class MeasurementService {
                 measurementDto.getName()
         );
         measurementRepository.save(measurement);
-        return new ApiResponse("Measurement saved",);
+        return new ApiResponse("Measurement saved", true);
+    }
+
+    public ApiResponse edit(Integer id, MeasurementDto measurementDto) {
+        if (!measurementRepository.existsById(id)) return new ApiResponse("Measurement not found", false);
+
+        Measurement measurement = measurementRepository.getById(id);
+        measurement.setName(measurementDto.getName());
+        measurementRepository.save(measurement);
+        return new ApiResponse("edited", true);
+
+    }
+
+    public ApiResponse get(Integer id) {
+        if (!measurementRepository.existsById(id)) return new ApiResponse("Measurement not found", false);
+
+        return new ApiResponse("found",true,measurementRepository.findById(id).get());
+    }
+
+    public ApiResponse getAll() {
+        return new ApiResponse("catch",true,measurementRepository.findAll());
+    }
+
+    public ApiResponse delete(Integer id) {
+        if (!measurementRepository.existsById(id)) return new ApiResponse("Measurement not found", false);
+
+        measurementRepository.deleteById(id);
+        return new ApiResponse("deleted",true);
+    }
+
+    public ApiResponse deleteAll() {
+        measurementRepository.deleteAll();
+        return new ApiResponse("measurements removed",true);
     }
 }
