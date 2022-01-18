7fcbf462a8c7f5fa0ac5398ae86267a62e7d8f0f
user <ibrohimjonyursunov50@gmail.com>
2022-01-18 14:21:41 +0500

    save PaymentStatus

--- src/main/java/uz/pdp/springsecurity/component/DataLoader.java
+++ src/main/java/uz/pdp/springsecurity/component/DataLoader.java
@@ -5,14 +5,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import uz.pdp.springsecurity.entity.PaymentMethod;
+import uz.pdp.springsecurity.entity.PaymentStatus;
 import uz.pdp.springsecurity.entity.Role;
 import uz.pdp.springsecurity.entity.User;
 import uz.pdp.springsecurity.enums.Permissions;
+import uz.pdp.springsecurity.repository.PaymentStatusRepository;
 import uz.pdp.springsecurity.repository.RoleRepository;
 import uz.pdp.springsecurity.repository.UserRepository;
 import uz.pdp.springsecurity.util.Constants;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -26,12 +30,15 @@ public class DataLoader implements CommandLineRunner {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    PaymentStatusRepository paymentStatusRepository;
+
     @Value("${spring.sql.init.mode}")
     private String initMode;
 
     @Override
     public void run(String... args) throws Exception {
-        if (initMode.equals("always")){
+        if (initMode.equals("always")) {
             Permissions[] permissions = Permissions.values();
             Role admin = roleRepository.save(new Role(
                     Constants.ADMIN,
@@ -46,6 +53,21 @@ public class DataLoader implements CommandLineRunner {
                     admin,
                     true
             ));
+
+        }
+        List<PaymentStatus> all = paymentStatusRepository.findAll();
+        if (all.isEmpty()){
+            paymentStatusRepository.save(new PaymentStatus(
+                    "To'langan"
+            ) );
+
+            paymentStatusRepository.save(new PaymentStatus(
+                    "Qisman to'langan"
+            ) );
+
+            paymentStatusRepository.save(new PaymentStatus(
+                    "To'lanmagan"
+            ) );
         }
     }
 }
