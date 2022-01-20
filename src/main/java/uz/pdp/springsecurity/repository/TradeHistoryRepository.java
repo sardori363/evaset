64f18a1a28e50fbc1e4f3c332ab23082423cbc02
sardor363 <sardorbekodiljonov363@gmail.com>
2022-01-20 10:19:52 +0500

    bla

--- src/main/java/uz/pdp/springsecurity/repository/TradeHistoryRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/TradeHistoryRepository.java
@@ -3,5 +3,8 @@ package uz.pdp.springsecurity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.springsecurity.entity.TradeHistory;
 
+import java.util.List;
+
 public interface TradeHistoryRepository extends JpaRepository<TradeHistory,Integer> {
+    List<TradeHistory> findAllByTrade_Id(Integer trade_id);
 }
