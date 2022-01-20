862c4ab3cd53b2ec8dedea0fb8f120566cd6b960
sardor363 <sardorbekodiljonov363@gmail.com>
2022-01-20 11:18:23 +0500

    some changes

--- src/main/java/uz/pdp/springsecurity/repository/TradeHistoryRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/TradeHistoryRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.springsecurity.entity.TradeHistory;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TradeHistoryRepository extends JpaRepository<TradeHistory,Integer> {
     List<TradeHistory> findAllByTrade_Id(Integer trade_id);
 
     void deleteAllByTrade_Id(Integer trade_id);
+
+    Optional<TradeHistory> findByIdAndTrade_Id(Integer id, Integer trade_id);
 }
