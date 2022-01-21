3bb6cde868a00155ebce45c5efb14e48a8d227fb
user <ibrohimjonyursunov50@gmail.com>
2022-01-21 16:48:11 +0500

    start ExchangeProductBranch

---
+++ src/main/java/uz/pdp/springsecurity/controller/ExchangeProductBranchController.java
@@ -0,0 +1,78 @@
+package uz.pdp.springsecurity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.springsecurity.aotations.CheckPermission;
+import uz.pdp.springsecurity.payload.ApiResponse;
+import uz.pdp.springsecurity.payload.ExchangeProductBranchDTO;
+import uz.pdp.springsecurity.service.ExchangeProductBranchService;
+
+
+@RestController
+@RequestMapping("/api/exchange-product-brach")
+public class ExchangeProductBranchController {
+    @Autowired
+    ExchangeProductBranchService exchangeProductBrancService;
+
+
+    @CheckPermission("ADD_TRADE")
+    @PostMapping
+    public HttpEntity<?> create(@RequestBody ExchangeProductBranchDTO exchangeProductBranchDTO) {
+        ApiResponse apiResponse = exchangeProductBrancService.create(exchangeProductBranchDTO);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @CheckPermission("EDIT_TRADE")
+    @PutMapping("/{id}")
+    public HttpEntity<?> edit(@PathVariable Integer id, @RequestBody  ExchangeProductBranchDTO exchangeProductBranchDTO) {
+        ApiResponse apiResponse = exchangeProductBrancService.edit(id, exchangeProductBranchDTO);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @CheckPermission("VIEW_ALL_TRADE")
+    @GetMapping
+    public HttpEntity<?> get() {
+        ApiResponse apiResponse = exchangeProductBrancService.getAll();
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @CheckPermission("VIEW_TRADE")
+    @GetMapping("/{id}")
+    public HttpEntity<?> get(@PathVariable Integer id) {
+        ApiResponse apiResponse = exchangeProductBrancService.getOne(id);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @CheckPermission("DELETE_TRADE")
+    @DeleteMapping("/{id}")
+    public HttpEntity<?> delete(@PathVariable Integer id) {
+        ApiResponse apiResponse = exchangeProductBrancService.deleteTrade(id);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+    @CheckPermission("DELETE_TRADE")
+    @DeleteMapping
+    public HttpEntity<?> deleteAll() {
+        ApiResponse apiResponse = exchangeProductBrancService.deleteAll();
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+
+    @CheckPermission("VIEW_ALL_TRADE")
+    @GetMapping("/get-by-branch-id/{branch_id}")
+    public HttpEntity<?> getByBranch(@PathVariable Integer branch_id) {
+        ApiResponse apiResponse = exchangeProductBrancService.shippedByBranchId(branch_id);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+
+    @CheckPermission("VIEW_ALL_TRADE")
+    @GetMapping("/get-by-exchange-statusId/{paymentStatus_id}")
+    public HttpEntity<?> getByPayStatus(@PathVariable Integer paymentStatus_id) {
+        ApiResponse apiResponse = exchangeProductBrancService.getByStatusId(paymentStatus_id);
+        return ResponseEntity.status(apiResponse.isSuccess() ? 200 : 409).body(apiResponse);
+    }
+
+}
---
+++ src/main/java/uz/pdp/springsecurity/entity/ExchangeProductBranch.java
@@ -0,0 +1,36 @@
+package uz.pdp.springsecurity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import uz.pdp.springsecurity.entity.template.AbsEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.sql.Date;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ExchangeProductBranch extends AbsEntity {
+
+    @OneToMany
+    private List<Branch> shippedBranch;
+
+    @ManyToOne
+    private Branch receivedBranch;
+
+    private Date exchangeDate;
+
+    private String description;
+
+    @OneToMany
+    private List<ExchangeProduct> exchangeProduct;
+
+    private String status;
+}
